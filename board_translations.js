36e8b7ebc3f5e706b5909b58df382f5d8a47592e
Patrik Langó <langopatrik99@gmail.com>
2026-07-08 14:49:33 +0200

    order

--- board_translations.js
+++ board_translations.js
@@ -13,35 +13,35 @@ const BOARD_TRANSLATIONS = `☀️ Paladín
 🐗 Esquivar contra troll enemigo te curará
 🐗 Tu defensa cambiará a número de nivel de troll enemigo al que te enfrentes
 🐸 Level -1 all your team if you die by enemy goblin
+🐸 Your hits ignore the defense of enemy goblin
+🐸 Your hits ignore the defense of enemy goblin
+🐸 Decrease your team agility when you miss against enemy goblin
 👁️ 100% chance of critical against enemy cyclops if you are the last player alive
 👁️ 66% chance of dodging enemy cyclops attacks when you are full hp
-👻 X0.5 damage received by enemy shadow when the current minute is odd
-🗡️ Rogue
-cyclops
-Mixed (Shadow, Goblin)
-troll
+👁️ X0.5 damage received by enemy cyclops when the current minute is odd
 👁️ Defeating enemy cyclops will invert the healthbar of a random teammate
 👁️ X4 chance of dodging enemy cyclops attacks when you are under 50% hp
-🐸 Your hits ignore the defense of enemy goblin
+👻 X0.5 damage received by enemy shadow when the current minute is odd
 👻 Defeating enemy shadow will invert the healthbar of a random teammate
-Mixed (Goblin, Shadow)
-🐸 Your hits ignore the defense of enemy goblin
 👻 Defeating enemy shadow will invert the healthbar of a random teammate
-👑 0% damage after hitting odd damage to any boss
-👑 Distribute health to your teammates the same damage you received from any boss
-🕷️ X0.5 chance of dodging enemy spider attacks
-🕷️ X0.75 enemy spider damage on you
-spider
-🐸 Decrease your team agility when you miss against enemy goblin
-👁️ X0.5 damage received by enemy cyclops when the current minute is odd
 👻 50% chance to kill enemy shadow with one hit if you have 10hp or less
 👻 Distribute health to your teammates the same damage you received from enemy shadow
 👻 Slightly heals your team everytime you hit enemy shadow
 👻 Your level will become the same level of enemy shadow you fight
-Mixed (Cyclops, Shadow)
-shadow
+👑 0% damage after hitting odd damage to any boss
+👑 Distribute health to your teammates the same damage you received from any boss
+🕷️ X0.5 chance of dodging enemy spider attacks
+🕷️ X0.75 enemy spider damage on you
 ⚡ Crit. hits deal x0.5 damage against more agile enemies
 ⚡ Your critical hits at more agile enemies will raise 2% the defense of your team
+🗡️ Rogue
+cyclops
+Mixed (Shadow, Goblin)
+troll
+Mixed (Goblin, Shadow)
+spider
+Mixed (Cyclops, Shadow)
+shadow
 
 
 `;
