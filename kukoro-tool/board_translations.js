a148e9e64cd043872b8fcf463c4773688d038e10
Patrik Langó <langopatrik99@gmail.com>
2026-07-09 18:22:20 +0200

    new strings

--- kukoro-tool/board_translations.js
+++ kukoro-tool/board_translations.js
@@ -31,6 +31,16 @@ const BOARD_TRANSLATIONS = `
 🐸 X0.5 damage received by enemy goblin when the current minute is odd
 🐸 Your defense will increase 10% each time you hit enemy goblin as long as you have full life
 
+🐸 -33% de vida antes de luchar contra goblin enemigo
+🐸 Daño x2 a goblin enemigo cuando tienes mucha vida
+🐸 Derrotar a goblin enemigo invertirá la vida de un compañero aleatorio
+🐸 Devuelve los golpes críticos a goblin enemigo
+🐸 Esquivar frente a goblin enemigo subirá 2% la defensa al equipo
+🐸 Goblin enemigo al que te enfrentes cambiará al mismo nivel que tú
+🐸 Morir por goblin enemigo invertirá la vida a todos tus compañeros
+🐸 Morir por goblin enemigo matará a un compañero al azar
+🐸 X1.25 de daño recibido por goblin enemigo
+
 👁️ 100% chance of critical against enemy cyclops if you are the last player alive
 👁️ 66% chance of dodging enemy cyclops attacks when you are full hp
 👁️ X0.5 damage received by enemy cyclops when the current minute is odd
@@ -46,6 +56,12 @@ const BOARD_TRANSLATIONS = `
 👁️ 100% de prob. de crítico a cíclope enemigo si eres el último jugador vivo
 👁️ Escaparás del combate si recibes un golpe crítico de cíclope enemigo
 👁️ Tus golpes ignoran la defensa de cíclope enemigo
+👁️ 50% prob. de golpe crítico a cíclope enemigo
+👁️ Daño x2 a cíclope enemigo tras asestarle un golpe de daño impar
+👁️ Defensa del 75% frente a cíclope enemigo si eres el último jugador vivo
+👁️ Intercambia la vida de cíclope enemigo con la tuya cuando empiezas el combate
+👁️ Morir por cíclope enemigo curará -50% la vida restante de tu equipo
+👁️ Reduce la agilidad de tu equipo cuando fallas contra cíclope enemigo
 
 👻 X0.5 damage received by enemy shadow when the current minute is odd
 👻 Defeating enemy shadow will invert the healthbar of a random teammate
@@ -53,6 +69,14 @@ const BOARD_TRANSLATIONS = `
 👻 Distribute health to your teammates the same damage you received from enemy shadow
 👻 Slightly heals your team everytime you hit enemy shadow
 👻 Your level will become the same level of enemy shadow you fight
+
+👻 Daño x1.5 a sombra enemigo
+👻 Derrotar a sombra enemigo invertirá la vida de un compañero aleatorio
+👻 Derrotar a sombra enemigo subirá 10% tu defensa
+👻 Fallar contra sombra enemigo bloqueará su próximo ataque
+👻 Los críticos a sombra enemigo aumentarán tu daño base
+👻 Reduce la agilidad de tu equipo cuando fallas contra sombra enemigo
+👻 Resiste los golpes mortales de sombra enemigo si tienes más de 1 de vida
 
 👑 0% damage after hitting odd damage to any boss
 👑 Distribute health to your teammates the same damage you received from any boss
@@ -70,6 +94,11 @@ const BOARD_TRANSLATIONS = `
 🕷️ Todo tu equipo recibirá nivel +3 si mueres por araña enemigo
 🕷️ Tu defensa frente a araña enemigo será del 100% mientras nunca hayas recibido daño
 🕷️ Un aliado aleatorio recibirá los golpes que falles contra araña enemigo
+🕷️ Araña enemigo al que te enfrentes cambiará al mismo nivel que tú
+🕷️ Intercambia la vida de araña enemigo con la tuya cuando empiezas el combate
+🕷️ Los críticos a araña enemigo aumentarán tu daño base
+🕷️ Los críticos de araña enemigo te hacen x2 de daño
+🕷️ Reduce la agilidad de tu equipo cuando fallas contra araña enemigo
 
 🐮 Dying by enemy minotaur will reverse all your teammates health
 🐮 Damage x3 against enemy minotaur if you are the last player alive
@@ -95,6 +124,12 @@ const BOARD_TRANSLATIONS = `
 🧜 Morir por naga enemigo curará -50% la vida restante de tu equipo
 🧜 Recibe nivel -2 cuando fallas contra naga enemigo
 🧜 X0.5 de daño recibido por naga enemigo si el minuto es impar
+🧜 0% de daño a naga enemigo tras asestar un golpe de daño impar
+🧜 66% prob. de esquivar los ataques de naga enemigo si tienes la vida completa
+🧜 Daño x3 a naga enemigo si tienes toda la vida
+🧜 Los críticos a naga enemigo aumentarán tu daño base
+🧜 Reduce a 0 la defensa de naga enemigo al que te enfrentes
+🧜 X0.75 de daño recibido por naga enemigo
 
 💀 0% prob. de golpe crítico a esqueleto enemigo
 💀 Daño x3 a esqueleto enemigo si tienes toda la vida
@@ -107,11 +142,28 @@ const BOARD_TRANSLATIONS = `
 
 🦠 3% prob. de matar limo enemigo de un golpe
 🦠 Siempre harás crítico a limo enemigo si tienes menos del 50% de la vida
+🦠 0% de daño a limo enemigo tras asestar un golpe de daño impar
+🦠 100% de prob. de crítico a limo enemigo si tienes vida impar
+🦠 Anula permanentemente la probabilidad de hacer críticos de limo enemigo
+🦠 Daño x2 a limo enemigo cuando tienes mucha vida
+🦠 Defenderás el doble mientras tengas poca vida frente a limo enemigo
+🦠 Derrotar a limo enemigo convertirá a las habilidades de un compañero al azar en superefectivas
+🦠 Morir por limo enemigo curará -50% la vida restante de tu equipo
+🦠 Morir por limo enemigo invertirá la vida a todos tus compañeros
+🦠 Morir por limo enemigo matará a un compañero al azar
+🦠 X0.5 de daño recibido por limo enemigo cuando tienes menos del 50% de la vida
 
 🐺 Always dodge the first attack of enemy wolf
 🐺 Defeating enemy wolf will increase your defense 10%
 🐺 Dodge 90% of enemy wolf's hits when you have 10hp or less
 🐺 Kill enemy wolf with one hit if you have 3hp or less
+
+🐺 100% de prob. de crítico a lobo enemigo si tienes vida impar
+🐺 Defenderás el doble mientras tengas poca vida frente a lobo enemigo
+🐺 Escaparás del combate si recibes un golpe crítico de lobo enemigo
+🐺 Escaparás del combate si tienes menos del 20% de vida frente a lobo enemigo
+🐺 Morir por lobo enemigo reestablecerá las habilidades de tus compañeros
+🐺 Todo tu equipo recibirá nivel -1 si mueres por lobo enemigo
 
 👹 50% chance of crit. hit against enemy oni
 👹 Your hits ignore the defense of enemy oni
@@ -137,6 +189,20 @@ const BOARD_TRANSLATIONS = `
 😈 X4 prob. de esquivar los ataques de diablillo enemigo si tienes menos del 50% de la vida
 😈 100% de prob. de crítico a diablillo enemigo si no ha muerto ningún jugador
 😈 Escaparás del combate si recibes un golpe crítico de diablillo enemigo
+😈 0% prob. de golpe crítico a diablillo enemigo
+😈 3% prob. de matar diablillo enemigo de un golpe
+😈 60% de prob. de esquivar a diablillo enemigo si eres el último jugador vivo
+😈 Daño x2 a diablillo enemigo cuando el minuto es par
+😈 Daño x2 a diablillo enemigo cuando tienes poca vida
+😈 Derrotar a diablillo enemigo te recuperará el 66% de vida
+😈 Devuelve los golpes críticos a diablillo enemigo
+😈 Los críticos a diablillo enemigo te curarán
+😈 Morir por diablillo enemigo curará -50% la vida restante de tu equipo
+😈 Morir por diablillo enemigo invertirá la vida a todos tus compañeros
+😈 Recibe nivel -2 cuando fallas contra diablillo enemigo
+😈 Siempre harás crítico a diablillo enemigo si tienes más del 50% de la vida
+😈 Tus golpes ignoran la defensa de diablillo enemigo
+😈 Un aliado aleatorio recibirá los golpes que falles contra diablillo enemigo
 
 🐲 100% de prob. de crítico a dragón enemigo si eres el último jugador vivo
 🐲 66% prob. de esquivar los ataques de dragón enemigo si tienes la vida completa
@@ -148,9 +214,26 @@ const BOARD_TRANSLATIONS = `
 🧟 Derrotar a zombi enemigo duplicará la defensa de un compañero aleatorio
 🧟 Reduce a 0 la agilidad de zombi enemigo
 🧟 Tu defensa aumentará 10% cada vez que golpees a zombi enemigo mientras tengas toda la vida
+🧟 Daño x3 a zombi enemigo si tienes toda la vida
+🧟 Escaparás del combate si tienes menos del 20% de vida frente a zombi enemigo
+🧟 Intercambia la vida de zombi enemigo con la tuya cuando empiezas el combate
+🧟 Morir por zombi enemigo matará a un compañero al azar
+🧟 Zombi enemigo al que te enfrentes cambiará al mismo nivel que tú
+
+🗿 100% de prob. de crítico a gárgola enemigo si tienes vida impar
+🗿 60% de prob. de esquivar a gárgola enemigo si eres el último jugador vivo
+🗿 Atacar primero contra gárgola enemigo hará x3 daño
+🗿 Daño x2 a gárgola enemigo tras asestarle un golpe de daño impar
+🗿 Intercambia la vida de gárgola enemigo con la tuya cuando empiezas el combate
+🗿 Recupera la vida antes de morir por gárgola enemigo a costa de absorber la vida de un compañero aleatorio
+🗿 Tu nivel se igualará a cualquier gárgola enemigo que luches
+🗿 X1.25 de daño recibido por gárgola enemigo
 
 👣 Dying by humanoid enemies also kills the opponent
 👣 Your defense will increase 10% each time you hit humanoid enemies as long as you have full life
+
+👣 3% prob. de matar enemigos humanoides de un golpe
+👣 El 66% del daño recibido por enemigos humanoides se reflejará a tu equipo
 
 🔮 Baja 5 niveles a enemigos malditos antes de luchar
 🔮 Derrotar a enemigos malditos convertirá a las habilidades de un compañero al azar en superefectivas
@@ -171,9 +254,16 @@ const BOARD_TRANSLATIONS = `
 🛡️ Derrotar a enemigos más resistentes invertirá la vida de un compañero aleatorio
 🛡️ Mata a enemigos más resistentes de un golpe si tienes 3hp o menos
 🛡️ Tus críticos harán x0.5 de daño a enemigos más resistentes
+🛡️ Daño x2 a enemigos más resistentes cuando el minuto es par
+🛡️ El 66% del daño recibido por enemigos más resistentes se reflejará a tu equipo
+🛡️ Esquivar contra enemigos más resistentes aumentará tu probabilidad de crítico
+🛡️ Morir por enemigos más resistentes matará a un compañero al azar
 
 ⚡ Crit. hits deal x0.5 damage against more agile enemies
 ⚡ Your critical hits at more agile enemies will raise 2% the defense of your team
+
+⚡ Daño x2 a enemigos más ágiles cuando el minuto es par
+⚡ Morir por enemigos más ágiles restaurará la vida completa al enemigo
 
 🗡️ Rogue
 🗡️ Pícaro
@@ -206,6 +296,10 @@ bestias
 dragón
 zombi
 cursed
+humanoides
+lobo
+rgola
+sombra
 Mixed (Esqueleto, Troll)
 Mixed (Goblin, Minotauro)
 Mixed (Shadow, Goblin)
@@ -221,6 +315,17 @@ Mixed (Diablillo, Naga)
 Mixed (Diablillo, Zombi)
 Mixed (Dragón, Naga)
 Mixed (Naga, Zombi)
+Mixed (A, Por)
+Mixed (Araña, Sombra)
+Mixed (Clope, Diablillo)
+Mixed (Clope, Limo)
+Mixed (Clope, Rgola)
+Mixed (Diablillo, Goblin)
+Mixed (Diablillo, Lobo)
+Mixed (Diablillo, Sombra)
+Mixed (Goblin, Lobo)
+Mixed (Goblin, Zombi)
+Mixed (Limo, Rgola)
 resistenses
 
 
